435376919c583943ea9341f87bce8afdda01e0d3
andewalle <awtudor@gmail.com>
2019-05-13 12:59:30 +0200

    first commit

---
+++ skitspel/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+<div id="app">
+  <HelloWorld/>
+
+  <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="6" offset="3">
+          <QuestionBox 
+                   
+          />
+          
+        </b-col>
+      </b-row>
+    </b-container>
+</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ skitspel/src/components/HelloWorld.vue
@@ -0,0 +1,86 @@
+	<template>
+	<div class="question-box-container">
+		<b-jumbotron>
+			<template slot="lead">
+				<p>Fråga</p>
+			</template>
+
+			<hr class="my-4">
+
+			<b-list-group>
+				<b-row class="formss">
+					<b-col sm="3">
+					<b-form-input v-model="text" placeholder="Enter bot answer"></b-form-input>
+					</b-col>
+					<b-col sm="3">
+					<b-form-input v-model="text" placeholder="Enter bot answer"></b-form-input>
+					</b-col>
+					<b-col sm="3">
+					<b-form-input v-model="text" placeholder="Enter your answer"></b-form-input>
+					</b-col>
+			  </b-row>
+			</b-list-group>
+			
+				<b-row class="imageRow"> 
+				<b-img src="http://www.freespins24h.se/wp-content/uploads/2017/02/faq.png" fluid alt="Responsive image"></b-img>
+				</b-row>
+			
+
+			<b-button class="submit">Submit</b-button>
+			<b-col class="rulesMenu">
+			<b-button class="rules">Rules</b-button>
+			</b-col>
+		</b-jumbotron>
+	</div>
+</template>
+
+	<script>
+export default {
+	name: "HelloWorld",
+	props: {
+		msg: String
+	},
+	data() {
+      return {
+        text: ''
+      }
+    }
+};
+</script>
+
+	<!-- Add "scoped" attribute to limit CSS to this component only -->
+	<style scoped>
+h3 {
+	margin: 40px 0 0;
+}
+ul {
+	list-style-type: none;
+	padding: 0;
+}
+li {
+	display: inline-block;
+	margin: 0 10px;
+}
+a {
+	color: #42b983;
+}
+
+.formss{
+	margin: 2rem;
+	margin-left: 12rem;
+	
+}
+
+.img{
+	width: 200px;
+}
+
+.rules{
+	
+	float:right;
+}
+
+.imageRow{
+	margin: 12rem;
+}
+</style>
